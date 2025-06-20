808ef0892cc8c5125866ed91ab03be4436cadfd9
YASHHTANWARRR <141021420+YASHHTANWARRR@users.noreply.github.com>
2025-06-20 06:26:29 +0530

    Update traffic_model.py

--- traffic_model.py
+++ traffic_model.py
@@ -6,11 +6,13 @@ import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 from ultralytics import YOLO
 from PIL import Image
+import shutil
+import random
 
-dataset1= 'Users/birba/OneDrive/Desktop/PARINDE/PARINDE/dataset/trafic_data'
-dataset2= 'Users/birba/OneDrive/Desktop/PARINDE/OBJECT-DETECTION/data'
+dataset1= [r'C:\Users\birba\OneDrive\Desktop\PARINDE\PARINDE\dataset\trafic_data\train']
+dataset2= [r'C:\Users\birba\OneDrive\Desktop\PARINDE\OBJECT-DETECTION\data']
 merged_folder = 'Users/birba/OneDrive/Desktop/PARINDE/PARINDE/output_merged'
-datatset_paths = [dataset1,dataset2]
+datatset_paths = [[dataset1,dataset2]]
 
 output_merged = merged_folder 
 splits = ['train','labels','test']
@@ -28,4 +30,25 @@ for dataset_path in datatset_paths:
     for fname in os.listdir(image_dir):
         if fname.endswith(".jpg") :
             imag_path = os.path.join(image_dir,fname)
+            label_path = os.path.join(image_dir,os.path.splitext(fname)[0]+".txt")
+            if os.path.exists(label_path):
+                all_data.append((imag_path,label_path))
+    
+random.shuffle(all_data)
+num_total = len(all_data)
+train_end = int(num_total * split_ratio[0])
+val_end = train_end + int(num_total * split_ratio[1])
+
+splits_data = {
+    'train': all_data[:train_end],
+    'val': all_data[train_end:val_end],
+    'test': all_data[val_end:]
+}
+
+for split, data in splits_data.items():
+    for img_path, label_path in data:
+        shutil.copy(img_path, f"{output_merged}/images/{split}/")
+        shutil.copy(label_path, f"{output_merged}/labels/{split}/")
+
+print("Datasets merged and split successfully.")             
             
